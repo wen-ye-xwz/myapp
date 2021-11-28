b6ec55ea7b8cabf36a68a4fe318a2fabbd1eac0b
Wen Ye <ywwendy1115@gmail.com>
2021-11-28 11:22:25 -0600

    first commit

---
+++ app.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+
+cs = read_csv("computer_science.csv")
+engineer = read_csv("humanities.csv")
+bus = read_csv("business.csv")
+arts = read_csv("arts.csv")
+hum = read_csv("humanities.csv")
+edu = read_csv("education.csv")
+
+ui <- fluidPage(
+  
+  titlePanel("Relationship between Early and Mid Career Salary by Major"),
+  
+  sidebarPanel(
+  
+    selectInput(inputId = "select", 
+                label = h3("Choose a Major Category"), 
+                choices = c("Computer Science" = "cs", "Engineering" = "engineer", 
+                               "Business" = "bus", "Arts" = "arts", "Humanities" = "hum", "Education" = "edu"), 
+                selected = "Computer Science"),
+    #hr(),
+    #fluidRow(column(6, verbatimTextOutput("value")))
+
+  ),
+  
+  
+  mainPanel(
+    
+    plotOutput(outputId = "Plot")
+    
+  )
+)
+
+server <- function(input, output) {
+  
+  #output$value <- renderPrint({ input$select })
+  
+  output$Plot <- renderPlot({
+    data_name = input$select
+    data = get(data_name)
+    #print(data)
+    data %>% ggplot()+
+      geom_point(aes(x = early_pay,y = mid_pay,color = Rank))+
+      xlab("Early Career Salary")+
+      ylab("Mid Career Salary")
+    
+  },height = 550, width = 900)
+  
+
+}
+
+shinyApp(ui = ui, server = server)
