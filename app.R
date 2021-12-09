ab0de152b61f9cd5c34c4afacd0bed3f2362fc66
Wen Ye <ywwendy1115@gmail.com>
2021-12-08 18:05:31 -0600

    modified point size

--- app.R
+++ app.R
@@ -43,9 +43,12 @@ server <- function(input, output) {
     data = get(data_name)
     #print(data)
     data %>% ggplot()+
-      geom_point(aes(x = early_pay,y = mid_pay,color = Rank))+
+      geom_point(aes(x = early_pay,y = mid_pay,color = Rank, size = 2.5))+
       xlab("Early Career Salary")+
-      ylab("Mid Career Salary")
+      ylab("Mid Career Salary")+ 
+      theme(axis.text.x = element_text(size=13),
+            axis.text.y = element_text(size=13),
+            axis.title = element_text(size = 20))
     
   },height = 550, width = 900)
   
